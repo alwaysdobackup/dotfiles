8c0e768b213169e0ebb92da587ef700ea44247f6
Volodymyr Tymchuk <volodymyr.tymchuk@outlook.com>
2024-12-28 14:44:13 +0000

    Initial hyprland

---
+++ hyprConfig/setup/001_install_packages.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+sudo pacman -S --noconfirm \
+git \
+neovim \
+intel-ucode \
+pipewire \
+networkmanager \
+base-devel \
+bluez \
+bluez-utils \
+btop \
+wireplumber \
+pipewire-pulse \
+playerctl \
+slurp \
+grim \
+brightnessctl \
+linux-firmware \
+jq \
+ttf-jetbrains-mono-nerd \
+greetd \
+greetd-tuigreet \
+hyprland \
+hyprpanel \
+
