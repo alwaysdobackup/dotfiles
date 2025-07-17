89ab0ce9aaae55f9e8fbc26ced474591927d0b2a
Tymchuk Volodymyr <26190753+alwaysdobackup@users.noreply.github.com>
2025-07-17 09:43:29 +0300

    Added nvidia tweaks for wayland

--- .config/setup/006_system_tweaks.sh
+++ .config/setup/006_system_tweaks.sh
@@ -10,6 +10,11 @@ echo "options hid_apple fnmode=2" | sudo tee /etc/modprobe.d/hid_apple.conf
 # Disable bluetooth autosuspend
 echo "options btusb enable_autosuspend=n" | sudo tee /etc/modprobe.d/btusb_disable_autosuspend.conf
 
+# nvidia-drm modeset=1: Enable DRM KMS (Kernel Mode Setting) for NVIDIA to improve Wayland and boot splash compatibility
+# nvidia-drm fbdev=1: Enable framebuffer device support for NVIDIA DRM to allow early boot console display
+# nvidia NVreg_PreserveVideoMemoryAllocations=1: Preserve video memory across suspend/resume to prevent screen corruption or crashes
+echo -e "options nvidia-drm modeset=1\noptions nvidia-drm fbdev=1\noptions nvidia NVreg_PreserveVideoMemoryAllocations=1" | sudo tee /etc/modprobe.d/nvidia.conf
+
 # Config file backup
 sudo cp /etc/bluetooth/main.conf /etc/bluetooth/main.conf.bak
 
@@ -17,5 +22,3 @@ sudo cp /etc/bluetooth/main.conf /etc/bluetooth/main.conf.bak
 sudo sed -i 's/#ReconnectAttempts=.*/ReconnectAttempts=3/' /etc/bluetooth/main.conf
 sudo sed -i 's/#ReconnectIntervals=.*/ReconnectIntervals=1,2,4/' /etc/bluetooth/main.conf
 sudo sed -i 's/#FastConnectable.=.*/FastConnectable=true/' /etc/bluetooth/main.conf
-
-
