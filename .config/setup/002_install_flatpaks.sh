39e974a3f3f06ec74d644175e3ca7b7e81d34011
Tymchuk Volodymyr <26190753+alwaysdobackup@users.noreply.github.com>
2025-07-19 16:03:28 +0300

    Remove unused packages

--- .config/setup/002_install_flatpaks.sh
+++ .config/setup/002_install_flatpaks.sh
@@ -4,11 +4,7 @@
 flatpak install flathub org.mozilla.firefox -y
 # Chromium
 flatpak install flathub org.chromium.Chromium -y
-# Telegram Desktop
-flatpak install flathub org.telegram.desktop -y
 # Spotify
 flatpak install flathub com.spotify.Client -y
 # Visual Studio Code
 flatpak install flathub com.visualstudio.code -y
-# Obsidian
-flatpak install flathub md.obsidian.Obsidian -y
